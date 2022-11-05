f24a25a35326799472776d5441735a789911b401
Omar Abdel-rahman <77195857+OmarAbdo14@users.noreply.github.com>
2022-11-05 22:51:24 +0200

    Update ProductsController.php

--- app/Http/Controllers/APIs/ProductsController.php
+++ app/Http/Controllers/APIs/ProductsController.php
@@ -65,6 +65,7 @@ class ProductsController extends Controller
             if($request->gallery!=null) {
                 foreach($request->gallery as $image) {
 //                     if($image) {
+                        return $image
                         $gallery[] = $this->saveFile($image, 'public/images/products');
 //                     }
                 }
